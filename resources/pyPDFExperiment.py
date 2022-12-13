6534f636dfb378fbf07459f6a48765c01943202b
Nathan <nathan@bluemesaengineering.com>
2022-12-13 23:48:54 +0100

    Feat: Adding functionality to PDF overlay writer. WIP.

--- resources/pyPDFExperiment.py
+++ resources/pyPDFExperiment.py
@@ -3,12 +3,36 @@ import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
-packet = io.BytesIO()
-can = canvas.Canvas(packet, pagesize=letter)
-can.drawString(10, 100, "Hello World")
-can.save()
+def setUpCanvas():
+  packet = io.BytesIO()
+  can = canvas.Canvas(packet, pagesize=letter)
+  writeSerialNumberAWBPDF(can)
+  writeShipper(can)
+  writeConsignee(can)
+  can.save()
+  packet.seek(0)
+  return packet
+  
+def writeSerialNumberAWBPDF(can):
+  can.setFont('Times-Roman', 8)
+  can.drawString(54, 356, "1043 0894 5389 1062")
 
-packet.seek(0)
+def writeShipper(can):
+  can.setFont('Times-Roman', 5)
+  can.drawString(30, 345, "Nathan D. Guthrie")
+  can.drawString(30, 340, "Blue Mesa Engineering")
+  can.drawString(30, 335, "101 Main St.")
+  can.drawString(30, 330, "Ste A")
+  can.drawString(30, 325, "Oregadaho, USA")
+
+def writeConsignee(can):
+  can.drawString(30, 315, "Barry White")
+  can.drawString(30, 310, "Jazzersize")
+  can.drawString(30, 305, "1 Love Song Lane")
+  can.drawString(30, 300, "Ste A")
+  can.drawString(30, 295, "Oregadaho, USA")
+
+packet = setUpCanvas()
 
 newPdf = PdfFileReader(packet)
 existingPdf = PdfFileReader(open("./assets/air-waybill-form.pdf", "rb"))
